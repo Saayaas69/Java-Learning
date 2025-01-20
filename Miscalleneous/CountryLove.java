113cbd07d825a29471a794f381ef580ba1e62546
Saayaas <122041700+Saayaas69@users.noreply.github.com>
2025-01-20 09:46:13 +0545

    Nepal

--- Miscalleneous/CountryLove.java
+++ Miscalleneous/CountryLove.java
@@ -3,7 +3,7 @@ package Miscalleneous;
 public class CountryLove {
   public static void main(String[] args) {
     for (int i = 0; i < 3; i++) {
-      System.out.println("We love our country");
+      System.out.println("We love our country Nepal");
     }
   }
 }
